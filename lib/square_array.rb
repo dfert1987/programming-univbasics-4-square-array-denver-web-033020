f8c2bcc3ab894970228b4e0792874e1e3b5215bd
David Fertitta <davidj.fertitta@gmail.com>
2020-03-17 17:38:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 array = [1, 2, 3]
 def square_array(array)
   new = []
+  #defines new array with squared numbers
   counter = 0
-  
+  #starts loop at first index
   while counter < array.length do
     new << array[counter] ** 2
     # this shovel adds a new element two our blank arrray (new)
