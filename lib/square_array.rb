94a2188352d8900a4ad4889920a5cfeb12e619a7
David Fertitta <davidj.fertitta@gmail.com>
2020-03-17 17:20:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 array = [1, 2, 3]
-def square_array(array)
+def quare_array(array)s
   counter = 0
   while counter < array.length do
   puts (array[counter]) ** 2
   counter += 1
 end
 end
+
+square_array(array)
